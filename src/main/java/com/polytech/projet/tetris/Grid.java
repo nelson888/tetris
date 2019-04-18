f15b3a94769d107f9524a0c62df68c375d04c32f
nelson888 <tambue@hotmail.fr>
2019-04-18 13:35:08 +0200

    did things

--- src/main/java/com/polytech/projet/tetris/Grid.java
+++ src/main/java/com/polytech/projet/tetris/Grid.java
@@ -4,17 +4,20 @@ public class Grid {
 
   private final int n;
   private final int m;
+  private int[][] grid;
 
   public Grid(int n, int m) {
     this.n = n;
     this.m = m;
+    grid = new int[n][m];
   }
 
   public int get(int i, int j) {
-    return  0;
+    return grid[i][j];
   }
 
-  public void set(int i, int j) {
-
+  public void set(int i, int j, int value){
+    if (i>=0 && i<)
+    this.grid[i][j] = value;
   }
 }
--- src/test/java/com/polytech/projet/tetris/GridTest.java
+++ src/test/java/com/polytech/projet/tetris/GridTest.java
@@ -2,5 +2,10 @@ package com.polytech.projet.tetris;
 
 public class GridTest {
 
+  @Test
+  void emptyGridTest(){
+    Grid testGrid = newGrid(2,2);
+    for (int i = 0; i<)
+  }
 
 }
