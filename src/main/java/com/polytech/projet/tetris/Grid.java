0665ca0a1b790e3e97cfaf3ec4cf03eed7314c25
headhunteerr <nicolas.mandadjiev@gmail.com>
2019-04-18 15:42:04 +0200

    Finished grid's first test

--- src/main/java/com/polytech/projet/tetris/Grid.java
+++ src/main/java/com/polytech/projet/tetris/Grid.java
@@ -2,21 +2,21 @@ package com.polytech.projet.tetris;
 
 public class Grid {
 
-  private final int n;
   private final int m;
+  private final int n;
   private int[][] grid;
 
-  public Grid(int n, int m) {
-    this.n = n;
+  public Grid(int m, int n) {
     this.m = m;
-    grid = new int[n][m];
+    this.n = n;
+    grid = new int[m][n];
   }
 
   public int get(int i, int j) {
     return grid[i][j];
   }
 
-  public void set(int i, int j, int value){
+  public void set(int i, int j, int value) {
     if (i>=0 && i<this.grid.length && j>=0 && j<this.grid[0].length){
       this.grid[i][j] = value;
     }
--- src/test/java/com/polytech/projet/tetris/GridTest.java
+++ src/test/java/com/polytech/projet/tetris/GridTest.java
@@ -1,11 +1,22 @@
 package com.polytech.projet.tetris;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class GridTest {
 
   @Test
-  void emptyGridTest(){
-    Grid testGrid = newGrid(2,2);
-    for (int i = 0; i<)
+  public void emptyGridTest(){
+    Grid testGrid = new Grid(2,2);
+    for (int i = 0; i<testGrid.getM(); i++){
+        for (int j = 0; j<testGrid.getN();j++){
+            assertEquals(0,testGrid.get(i,j));
+        }
+    }
+  }
+
+  @Test
+  public void setOneCell(){
+
   }
 
 }
