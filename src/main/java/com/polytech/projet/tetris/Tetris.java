7ed60dd220ef4cee9f6d6f45ffcba1aed8a552d3
headhunteerr <nicolas.mandadjiev@gmail.com>
2019-04-18 17:23:27 +0200

    Update Tetris.java

--- src/main/java/com/polytech/projet/tetris/Tetris.java
+++ src/main/java/com/polytech/projet/tetris/Tetris.java
@@ -10,14 +10,27 @@ public class Tetris extends Grid {
   }
 
   public void nextFrame() {
-      for(int i = 0; i< getN(); i++){
+      for(int i = getN()-1; i>=0; i--){
           if(isLineFilled(i)){
-              emptyLine(i);
+              dropEverythingFrom(i);
+          }
+          else if (isLinePartiallyFilled(i)){
+              for (int j = i+1; j < getM(); j++) {
+                  if (isLineEmpty(j)){
+
+                  }
+              }
           }
       }
   }
 
-  private boolean isLineEmpty(int i){
+  private void dropEverythingFrom(int i) {
+      for (int j = i-1; j>=0; j--) {
+
+      }
+  }
+
+    private boolean isLineEmpty(int i){
       for(int j = 0; j< getM();j++){
           if (get(i,j) == FILLED){
               return false;
@@ -33,6 +46,10 @@ public class Tetris extends Grid {
       }
     }
     return true;
+  }
+
+  private boolean isLinePartiallyFilled(int i){
+      return !isLineEmpty(i) && !isLineFilled(i);
   }
 
   private void emptyLine(int i){
