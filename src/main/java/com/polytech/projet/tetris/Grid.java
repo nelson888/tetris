9509a0f1b57eb83c161a0a9179faae084762f804
nelson888 <tambue@hotmail.fr>
2019-04-18 12:01:53 +0200

    corrected grid

--- src/main/java/com/polytech/projet/tetris/Grid.java
+++ src/main/java/com/polytech/projet/tetris/Grid.java
@@ -3,9 +3,11 @@ package com.polytech.projet.tetris;
 public class Grid {
 
   private final int n;
+  private final int m;
 
-  public Grid(int n) {
+  public Grid(int n, int m) {
     this.n = n;
+    this.m = m;
   }
 
   public int get(int i, int j) {
--- src/test/java/com/polytech/projet/tetris/GridTest.java
+++ src/test/java/com/polytech/projet/tetris/GridTest.java
@@ -2,17 +2,5 @@ package com.polytech.projet.tetris;
 
 public class GridTest {
 
-  private final int n;
-  private final boolean[][] grid;
-
-  public GridTest(int n) {
-    this.n = n;
-    grid = new boolean[n][n];
-  }
-
-  public void nextFrame() {
-
-  }
-
 
 }
